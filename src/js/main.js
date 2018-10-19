a62427ee5c8ad83665dca4c6a3a4782bff23647b
Dankgard <dangar18@ucm.es>
2018-10-20 00:36:09 +0200

    Creada estructura basica de clases

--- src/js/main.js
+++ src/js/main.js
@@ -40,3 +40,139 @@ window.onload = function () {
 
   game.state.start('boot');
 };
+
+class GameManager {
+  constructor(turn, playingPlayer)
+  {
+    this.turn = turn;
+    this.playingPlayer = playingPlayer;
+  }
+}
+
+class Player {
+  constructor(number, money)
+  {
+    this.number = number;
+    this.money = money;
+  }
+}
+
+class Square  {
+  constructor(x, y, type, sprite)
+  {
+    this.x = x;
+    this.y = y;
+    this.type = type; // nothing, forest, mine, ruins
+    this.sprite = sprite;
+  }
+}
+
+class Building {
+  constructor(x, y, hp, player)
+  {
+    this.x = x;
+    this.y = y;
+    this.hp = hp;
+    this.sprite = sprite;
+    this.player = player;
+  }
+}
+
+class Town extends Building {
+  constructor()
+  {
+
+  }
+
+  createUnit()
+  {
+
+  }
+}
+
+class Wall extends Building {
+  constructor()
+  {
+
+  }
+}
+
+class Watchtower extends Building {
+  constructor(range,damage)
+  {
+    this.range = range;
+    this.damage = damage;
+  }
+
+  attack()
+  {
+
+  }
+}
+
+class Unit {
+  constructor(x, y, hp, moves, sprite, player)
+  {
+    this.x = x;
+    this.y = y;
+    this.hp = hp;
+    this.moves = moves;
+    this.sprite = sprite;
+    this.player = player;
+  }
+}
+
+class Worker extends Unit {
+  constructor()
+  {
+
+  }
+
+  work()
+  {
+
+  }
+
+  build()
+  {
+
+  }
+}
+
+class CombatUnit extends Unit {
+  constructor(range, damage, element)
+  {
+    this.range = range;
+    this.damage = damage;
+    this.element = element;
+  }
+
+  attack()
+  {
+
+  }
+}
+
+class Archer extends CombatUnit {
+  constructor()
+  {
+
+  }
+}
+
+class Infantry extends CombatUnit {
+  constructor()
+  {
+
+  }
+}
+
+class Cavalry extends CombatUnit {
+  constructor()
+  {
+
+  }
+}
+
+
+
